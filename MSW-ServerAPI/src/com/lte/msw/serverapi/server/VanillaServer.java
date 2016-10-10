75ae52a8000b83d9cd592a846f21ff2d2593cd26
Laurence <laurence.trippen@gmx.de>
2016-10-10 22:29:02 +0200

    little implementation of serverapi

--- MSW-ServerAPI/src/com/lte/msw/serverapi/server/VanillaServer.java
+++ MSW-ServerAPI/src/com/lte/msw/serverapi/server/VanillaServer.java
@@ -1,5 +1,14 @@
 package com.lte.msw.serverapi.server;
 
+import com.lte.msw.serverapi.server.config.ServerProperties;
+import com.lte.msw.serverapi.server.model.enums.ServerState;
+import com.lte.msw.serverapi.server.version.Version;
+
 public class VanillaServer {
+	
+	private String name;
+	private Version version;
+	private ServerState serverState;
+	private ServerProperties serverProperties;
 
 }
---
+++ MSW-ServerAPI/src/com/lte/msw/serverapi/server/model/interfaces/IPropertiesController.java
@@ -0,0 +1,8 @@
+package com.lte.msw.serverapi.server.model.interfaces;
+
+public interface IPropertiesController {
+	
+	void save();
+	void load();
+
+}
