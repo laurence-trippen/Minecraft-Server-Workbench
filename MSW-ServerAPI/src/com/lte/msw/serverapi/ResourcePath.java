d0acc7eb40317da7b447e95da6702636d447ac0f
Laurence <laurence.trippen@gmx.de>
2016-10-10 22:50:38 +0200

    adding pathes

---
+++ MSW-ServerAPI/src/com/lte/msw/serverapi/ResourcePath.java
@@ -0,0 +1,17 @@
+package com.lte.msw.serverapi;
+
+public class ResourcePath {
+	
+	public static final String SERVER_DIRECTORY 	= "working/serverpool/";
+	public static final String EULA					= "/eula.txt";
+	public static final String OPS 					= "/ops.json";
+	public static final String WHITELIST 			= "/whitelist.json";
+	public static final String BANNED_IPS 			= "/banned-ips.json";
+	public static final String BANNED_PLAYERS 		= "/banned-players.json";
+	public static final String USERCACHE			= "/usercache.json";
+	public static final String PROPERTIES			= "/server.properties";
+	public static final String WORLD				= "/world";
+	public static final String LOGS					= "/logs";
+	public static final String LATEST_LOG			= "/logs/latest.log";
+
+}
