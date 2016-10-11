980a2ce09157b9611e4974e848fbe74e7f05a3d7
Laurence Trippen <laurence.trippen@topcart.com>
2016-10-11 13:23:20 +0200

    add new stuff to server a

--- MSW-ServerAPI/src/com/lte/msw/serverapi/server/VanillaServer.java
+++ MSW-ServerAPI/src/com/lte/msw/serverapi/server/VanillaServer.java
@@ -1,14 +1,87 @@
 package com.lte.msw.serverapi.server;
 
+import com.lte.msw.serverapi.server.config.BannedIPAdresses;
+import com.lte.msw.serverapi.server.config.BannedPlayers;
 import com.lte.msw.serverapi.server.config.ServerProperties;
+import com.lte.msw.serverapi.server.config.Whitelist;
 import com.lte.msw.serverapi.server.model.enums.ServerState;
+import com.lte.msw.serverapi.server.model.interfaces.IServerController;
 import com.lte.msw.serverapi.server.version.Version;
 
-public class VanillaServer {
+public class VanillaServer implements IServerController {
 	
 	private String name;
 	private Version version;
 	private ServerState serverState;
 	private ServerProperties serverProperties;
+	private Whitelist Whitelist;
+	private BannedPlayers bannedPlayers;
+	private BannedIPAdresses bannedIPAdresses;
+	
+	@Override
+	public void start() {
+		
+	}
+	
+	@Override
+	public void stop() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Version getVersion() {
+		return version;
+	}
+
+	public void setVersion(Version version) {
+		this.version = version;
+	}
+
+	public ServerState getServerState() {
+		return serverState;
+	}
+
+	public void setServerState(ServerState serverState) {
+		this.serverState = serverState;
+	}
+
+	public ServerProperties getServerProperties() {
+		return serverProperties;
+	}
+
+	public void setServerProperties(ServerProperties serverProperties) {
+		this.serverProperties = serverProperties;
+	}
+	
+	public Whitelist getWhitelist() {
+		return Whitelist;
+	}
+
+	public void setWhitelist(Whitelist whitelist) {
+		Whitelist = whitelist;
+	}
+
+	public BannedPlayers getBannedPlayers() {
+		return bannedPlayers;
+	}
+
+	public void setBannedPlayers(BannedPlayers bannedPlayers) {
+		this.bannedPlayers = bannedPlayers;
+	}
+
+	public BannedIPAdresses getBannedIPAdresses() {
+		return bannedIPAdresses;
+	}
+
+	public void setBannedIPAdresses(BannedIPAdresses bannedIPAdresses) {
+		this.bannedIPAdresses = bannedIPAdresses;
+	}
 
 }
--- MSW-ServerAPI/src/com/lte/msw/serverapi/server/config/Whitelist.java
+++ MSW-ServerAPI/src/com/lte/msw/serverapi/server/config/Whitelist.java
@@ -1,5 +1,30 @@
 package com.lte.msw.serverapi.server.config;
 
-public class Whitelist {
+import java.util.List;
 
+import com.lte.msw.serverapi.server.model.Player;
+import com.lte.msw.serverapi.server.model.interfaces.IPlayerController;
+
+public class Whitelist implements IPlayerController {
+	
+	private List<Player> playerWhitelist;
+	
+	@Override
+	public void addPlayer(Player player) {
+		
+	}
+	
+	@Override
+	public void removePlayer(Player player) {
+		
+	}
+	
+	public List<Player> getPlayerWhitelist() {
+		return playerWhitelist;
+	}
+
+	public void setPlayerWhitelist(List<Player> playerWhitelist) {
+		this.playerWhitelist = playerWhitelist;
+	}
+	
 }
