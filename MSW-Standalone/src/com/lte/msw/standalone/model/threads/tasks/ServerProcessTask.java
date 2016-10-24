625d275821e8822c7cdfdf31eebc2b9668308e81
Laurence Trippen <laurence.trippen@topcart.com>
2016-10-24 12:58:33 +0200

    create for test ServerProcessTask.java

---
+++ MSW-Standalone/src/com/lte/msw/standalone/model/threads/tasks/ServerProcessTask.java
@@ -0,0 +1,13 @@
+package com.lte.msw.standalone.model.threads.tasks;
+
+import javafx.concurrent.Task;
+
+public class ServerProcessTask extends Task<Void> {
+	
+	@Override
+	protected Void call() throws Exception {
+		
+		return null;
+	}
+	
+}
