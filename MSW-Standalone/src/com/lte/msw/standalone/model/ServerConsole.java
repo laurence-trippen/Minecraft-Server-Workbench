176f0a8de9b809cad4335f36081cd3eeac59aa53
LTE-01 <LTE-01@LTE-01-PC.localdomain>
2016-12-11 14:22:04 +0100

    updating progress

--- MSW-Standalone/src/com/lte/msw/standalone/model/ServerConsole.java
+++ MSW-Standalone/src/com/lte/msw/standalone/model/ServerConsole.java
@@ -1,5 +1,44 @@
 package com.lte.msw.standalone.model;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ServerConsole {
+	
+	private String consoleLog;
+	private InputStream consoleInputStream;
+	private OutputStream consoleOutputStream;
+	
+	public ServerConsole(InputStream inputStream) {
+		this.consoleInputStream = inputStream;
+	}
+	
+	public void start() {
+		
+	}
+
+	public String getConsoleLog() {
+		return consoleLog;
+	}
+
+	public void setConsoleLog(String consoleLog) {
+		this.consoleLog = consoleLog;
+	}
+
+	public InputStream getConsoleInputStream() {
+		return consoleInputStream;
+	}
+
+	public void setConsoleInputStream(InputStream consoleInputStream) {
+		this.consoleInputStream = consoleInputStream;
+	}
+
+	public OutputStream getConsoleOutputStream() {
+		return consoleOutputStream;
+	}
+
+	public void setConsoleOutputStream(OutputStream consoleOutputStream) {
+		this.consoleOutputStream = consoleOutputStream;
+	}
 	
 }
