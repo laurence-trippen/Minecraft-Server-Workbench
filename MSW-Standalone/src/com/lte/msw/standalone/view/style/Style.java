48ec8632b6c12b81dc38e6dcc9b4fdce2912077a
Turbo-Assembler <laurence.trippen@gmx.de>
2017-01-29 17:15:40 +0100

    little changes in Style.java

--- MSW-Standalone/src/com/lte/msw/standalone/view/style/Style.java
+++ MSW-Standalone/src/com/lte/msw/standalone/view/style/Style.java
@@ -6,6 +6,6 @@ public class Style {
 	public static final String ERROR_BORDER			= "-fx-border-color: red; -fx-border-radius: 3";
 	public static final String LEFT_RADIUS 			= "-fx-background-radius: 4 0 0 4";
 	public static final String RIGHT_RADIUS			= "-fx-background-radius: 0 4 4 0";
-	public static final String BUTTON_NON_RADIUS		= "-fx-background-radius: 0 0 0 0";
+	public static final String BUTTON_NON_RADIUS	= "-fx-background-radius: 0 0 0 0";
 
 }
