383526131971905e40099e03ccdb678ffb7dc4e5
Laurence Trippen <laurence.trippen@topcart.com>
2017-02-24 15:29:44 +0100

    code formatting 2

--- MSW-Standalone/src/com/lte/msw/standalone/view/scenes/ServerScene.java
+++ MSW-Standalone/src/com/lte/msw/standalone/view/scenes/ServerScene.java
@@ -32,47 +32,47 @@ import javafx.scene.text.FontWeight;
 
 public class ServerScene extends MSWScene implements IRefreshable {
 
-	private AnchorPane 					mainPane;
-	private Pane 						serverPane;
-	private Pane						consolePane;
-	private Pane						vboxPane;
-	private HBox						hBox;
-	private VBox						vBox;
-	private Server 						server;
-	private ToolBar 					toolBar;
-	private Button 						closeButton;
-	private Button						consoleExecuteButton;
-	private ImageView					serverImageView;
-	private ImageView					serverGameModeImageView;
-	private ImageView					serverDifficultyImageView;
-	private ImageView					serverPvpImageView;
-	private Label						serverNameLabel;
-	private Label						serverVersionLabel;
-	private Label						serverGameModeLabel;
-	private Label						serverDifficultyLabel;
-	private Label						serverPvpLabel;
-	private Label						serverGameModeValue;
-	private Label						serverDifficultyValue;
-	private Label 						serverPvpValue;
-	private Label 						serverAdressLabel;
-	private Label						consoleLabel;
-	private Button						serverControlButton;
-	private Button						serverConfigureButton;
-	private Button						serverBackupButton;
-	private Button						serverLogButton;
-	private TextArea					consoleTextArea;
-	private TextField					consoleTextField;
-	
+	private AnchorPane mainPane;
+	private Pane serverPane;
+	private Pane consolePane;
+	private Pane vboxPane;
+	private HBox hBox;
+	private VBox vBox;
+	private Server server;
+	private ToolBar toolBar;
+	private Button closeButton;
+	private Button consoleExecuteButton;
+	private ImageView serverImageView;
+	private ImageView serverGameModeImageView;
+	private ImageView serverDifficultyImageView;
+	private ImageView serverPvpImageView;
+	private Label serverNameLabel;
+	private Label serverVersionLabel;
+	private Label serverGameModeLabel;
+	private Label serverDifficultyLabel;
+	private Label serverPvpLabel;
+	private Label serverGameModeValue;
+	private Label serverDifficultyValue;
+	private Label serverPvpValue;
+	private Label serverAdressLabel;
+	private Label consoleLabel;
+	private Button serverControlButton;
+	private Button serverConfigureButton;
+	private Button serverBackupButton;
+	private Button serverLogButton;
+	private TextArea consoleTextArea;
+	private TextField consoleTextField;
+
 	public ServerScene() {
 		super(new AnchorPane(), DesktopManager.getScreenSize().getWidth(), DesktopManager.getScreenSize().getHeight());
 		this.initNodes();
 		this.defineNodes();
 		this.registerNodeEvents();
 	}
-	
+
 	@Override
 	protected void initNodes() {
-		this.mainPane = (AnchorPane)this.getRoot();
+		this.mainPane = (AnchorPane) this.getRoot();
 		this.consoleLabel = new Label("Server Konsole:");
 		this.serverNameLabel = new Label();
 		this.serverAdressLabel = new Label();
@@ -103,12 +103,12 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.getStylesheets().clear();
 		this.getStylesheets().add(getClass().getResource(ResourcePath.CSS).toExternalForm());
 		try {
-			mainPane.setBackground(new Background(
-					new BackgroundImage(new Image(new FileInputStream(ResourcePath.BACKGROUND)), null, null, null, null)
-			));
+			mainPane.setBackground(new Background(new BackgroundImage(
+					new Image(new FileInputStream(ResourcePath.BACKGROUND)), null, null, null, null)));
 			this.serverImageView = new ImageView(new Image(new FileInputStream(ResourcePath.SERVER_72_PNG)));
 			this.serverGameModeImageView = new ImageView(new Image(new FileInputStream(ResourcePath.GAMEMODE_32_PNG)));
-			this.serverDifficultyImageView = new ImageView(new Image(new FileInputStream(ResourcePath.DIFFICULTY_32_PNG)));
+			this.serverDifficultyImageView = new ImageView(
+					new Image(new FileInputStream(ResourcePath.DIFFICULTY_32_PNG)));
 			this.serverPvpImageView = new ImageView(new Image(new FileInputStream(ResourcePath.PVP_32_PNG)));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -127,10 +127,10 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.serverVersionLabel.setFont(Font.font("System", 20));
 		this.serverVersionLabel.setLayoutX(180);
 		this.serverVersionLabel.setLayoutY(100);
-		
+
 		this.serverGameModeImageView.setLayoutX(60);
 		this.serverGameModeImageView.setLayoutY(185);
-		
+
 		this.serverGameModeLabel.setFont(Font.font("System", FontWeight.BOLD, 18));
 		this.serverGameModeLabel.setLayoutX(110);
 		this.serverGameModeLabel.setLayoutY(190);
@@ -138,7 +138,7 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.serverGameModeValue.setFont(Font.font("System", 18));
 		this.serverGameModeValue.setLayoutX(330);
 		this.serverGameModeValue.setLayoutY(190);
-		
+
 		this.serverDifficultyImageView.setLayoutX(60);
 		this.serverDifficultyImageView.setLayoutY(235);
 
@@ -149,7 +149,7 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.serverDifficultyValue.setFont(Font.font("System", 18));
 		this.serverDifficultyValue.setLayoutX(330);
 		this.serverDifficultyValue.setLayoutY(240);
-		
+
 		this.serverPvpImageView.setLayoutX(60);
 		this.serverPvpImageView.setLayoutY(285);
 
@@ -164,26 +164,11 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.serverPane.getStyleClass().add("msw-white-pane");
 		this.serverPane.setPrefWidth(800);
 		this.serverPane.setPrefHeight(460);
-		this.serverPane.getChildren().addAll(
-				serverImageView, 
-				serverNameLabel,
-				serverVersionLabel,
-				serverGameModeLabel,
-				serverDifficultyLabel,
-				serverPvpLabel,
-				serverGameModeImageView,
-				serverDifficultyImageView,
-				serverPvpImageView,
-				serverGameModeValue,
-				serverDifficultyValue,
-				serverPvpValue,
-				serverAdressLabel,
-				serverControlButton,
-				serverConfigureButton,
-				serverBackupButton,
-				serverLogButton
-		);
-		
+		this.serverPane.getChildren().addAll(serverImageView, serverNameLabel, serverVersionLabel, serverGameModeLabel,
+				serverDifficultyLabel, serverPvpLabel, serverGameModeImageView, serverDifficultyImageView,
+				serverPvpImageView, serverGameModeValue, serverDifficultyValue, serverPvpValue, serverAdressLabel,
+				serverControlButton, serverConfigureButton, serverBackupButton, serverLogButton);
+
 		this.consoleLabel.setFont(Font.font("System", FontWeight.BOLD, 14));
 		this.consoleLabel.setLayoutX(30);
 		this.consoleLabel.setLayoutY(30);
@@ -209,51 +194,46 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		this.consolePane.setLayoutY(500);
 		this.consolePane.setPrefWidth(800);
 		this.consolePane.setPrefHeight(300);
-		this.consolePane.getChildren().addAll(
-				consoleTextArea,
-				consoleTextField,
-				consoleLabel,
-				consoleExecuteButton
-		);
+		this.consolePane.getChildren().addAll(consoleTextArea, consoleTextField, consoleLabel, consoleExecuteButton);
 		this.serverLogButton.getStyleClass().add("msw-right-border-radius");
 		this.serverLogButton.setPrefWidth(150);
 		this.serverLogButton.setPrefHeight(25);
 		this.serverLogButton.setLayoutX(550);
 		this.serverLogButton.setLayoutY(370);
-		
+
 		this.serverBackupButton.getStyleClass().add("msw-none-border-radius");
 		this.serverBackupButton.setPrefWidth(150);
 		this.serverBackupButton.setPrefHeight(25);
 		this.serverBackupButton.setLayoutX(400);
 		this.serverBackupButton.setLayoutY(370);
-		
+
 		this.serverConfigureButton.getStyleClass().add("msw-left-border-radius");
 		this.serverConfigureButton.setPrefWidth(150);
 		this.serverConfigureButton.setPrefHeight(25);
 		this.serverConfigureButton.setLayoutX(250);
 		this.serverConfigureButton.setLayoutY(370);
-		
+
 		this.serverControlButton.setPrefWidth(140);
 		this.serverControlButton.setPrefHeight(25);
 		this.serverControlButton.setLayoutX(60);
 		this.serverControlButton.setLayoutY(370);
-		
+
 		this.toolBar = new ToolBar();
 		this.toolBar.setLayoutX(0.00);
 		this.toolBar.setLayoutY(0.00);
 		this.toolBar.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
 		this.toolBar.getItems().add(closeButton);
-		
+
 		this.vboxPane.setPrefHeight(800);
 		this.vboxPane.getChildren().addAll(serverPane, consolePane);
-		
+
 		this.vBox.setAlignment(Pos.CENTER);
 		this.vBox.setPrefWidth(800);
 		this.vBox.getChildren().add(vboxPane);
-		
+
 		this.hBox.setAlignment(Pos.CENTER);
 		this.hBox.getChildren().add(vBox);
-		
+
 		mainPane.getChildren().addAll(hBox, toolBar);
 		AnchorPane.setLeftAnchor(toolBar, 0.00);
 		AnchorPane.setTopAnchor(toolBar, 0.00);
@@ -273,7 +253,7 @@ public class ServerScene extends MSWScene implements IRefreshable {
 
 	@Override
 	public void refresh() {
-		
+
 	}
 
 	public void setServer(Server server) {
@@ -312,19 +292,20 @@ public class ServerScene extends MSWScene implements IRefreshable {
 		}
 		this.serverPvpLabel.setText("Player vs. Player:");
 		this.serverPvpValue.setText(server.getServerProperties().isEnabledPVP() ? "Aktiviert" : "Deaktiviert");
-		this.serverAdressLabel.setText(NetworkManager.getMyIPAdress() + ":" + server.getServerProperties().getServerPort());
+		this.serverAdressLabel
+				.setText(NetworkManager.getMyIPAdress() + ":" + server.getServerProperties().getServerPort());
 	}
-	
+
 	private void onCloseEvent(ActionEvent event) {
 		MSWStandalone.getMainStage().setScene(SceneManager.getSceneManager().getMainScene());
 		refresh();
 	}
-	
+
 	private void onConsoleExecuteEvent(ActionEvent event) {
 		System.out.println("Execeute: " + consoleTextField.getText());
 		consoleTextField.setText(null);
 	}
-	
+
 	private void onServerStartEvent(ActionEvent event) {
 		server.start();
 	}
