fc0f155bf0416f26d0e7be109293935e88dea9a6
Turbo-Assembler <laurence.trippen@gmx.de>
2017-03-06 07:30:46 +0100

    map member in MapScene.java

--- MSW-Standalone/src/com/lte/msw/standalone/view/scenes/MapScene.java
+++ MSW-Standalone/src/com/lte/msw/standalone/view/scenes/MapScene.java
@@ -1,11 +1,14 @@
 package com.lte.msw.standalone.view.scenes;
 
 import com.lte.msw.standalone.manager.DesktopManager;
+import com.lte.msw.standalone.model.Map;
 
 import javafx.scene.layout.AnchorPane;
 
 public class MapScene extends MSWScene {
-
+	
+	private Map map;
+	
 	public MapScene() {
 		super(new AnchorPane(), DesktopManager.getScreenSize().getWidth(), DesktopManager.getScreenSize().getHeight());
 		this.initNodes();
@@ -26,6 +29,10 @@ public class MapScene extends MSWScene {
 	@Override
 	protected void registerNodeEvents() {
 		
+	}
+	
+	public void setMap(Map map) {
+		this.map = map;
 	}
 
 }
