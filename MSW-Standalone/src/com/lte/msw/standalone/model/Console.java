9ed331fb2b4530b0587bdb10353a5f48f24212c6
Laurence Trippen <laurence.trippen@topcart.com>
2017-03-09 13:02:02 +0100

    little future changes for tempory console

--- MSW-Standalone/src/com/lte/msw/standalone/model/Console.java
+++ MSW-Standalone/src/com/lte/msw/standalone/model/Console.java
@@ -5,41 +5,30 @@ import java.io.OutputStream;
 
 public class Console {
 	
-	private String consoleLog;
+	private StringBuilder consoleCache;
 	private InputStream consoleInputStream;
 	private OutputStream consoleOutputStream;
 	
 	public Console(InputStream inputStream, OutputStream outputStream) {
+		this.consoleCache = new StringBuilder();
 		this.consoleInputStream = inputStream;
 		this.consoleOutputStream = outputStream;
 	}
 	
-	public void start() {
+	public void run() {
 		
 	}
-
-	public String getConsoleLog() {
-		return consoleLog;
-	}
-
-	public void setConsoleLog(String consoleLog) {
-		this.consoleLog = consoleLog;
+	
+	public String getConsoleText() {
+		return consoleCache.toString();
 	}
 
 	public InputStream getConsoleInputStream() {
 		return consoleInputStream;
 	}
 
-	public void setConsoleInputStream(InputStream consoleInputStream) {
-		this.consoleInputStream = consoleInputStream;
-	}
-
 	public OutputStream getConsoleOutputStream() {
 		return consoleOutputStream;
-	}
-
-	public void setConsoleOutputStream(OutputStream consoleOutputStream) {
-		this.consoleOutputStream = consoleOutputStream;
 	}
 	
 }
